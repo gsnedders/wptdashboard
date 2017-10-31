96707fdd9fb03c571fb18ab2cda6dd582e6b6850
Jeff Carpenter <gcarpenterv@gmail.com>
2017-10-31 00:20:01 -0700

    Add hosts_fixup back to CI

--- ci_container.sh
+++ ci_container.sh
@@ -1,10 +1,21 @@
 IMAGE_NAME=wptd-testrun-jenkins
 
+if [ -z "${SAUCE_USER}" ]; then
+  echo "SAUCE_USER env var required."
+  exit
+fi
+if [ -z "${SAUCE_KEY}" ]; then
+  echo "SAUCE_KEY env var required."
+  exit
+fi
+
 # Build the image
 docker build -t $IMAGE_NAME .
 
 # Run a small directory in FF and Edge (Sauce)
 docker run \
   -p 4445:4445 \
-  --entrypoint "/usr/bin/bash" $IMAGE_NAME \
+  -e "SAUCE_USER=$SAUCE_USER" \
+  -e "SAUCE_KEY=$SAUCE_KEY" \
+  --entrypoint "/bin/bash" $IMAGE_NAME \
   /wptdashboard/util/ci_container_inner.sh
--- util/ci_container_inner.sh
+++ util/ci_container_inner.sh
@@ -1,12 +1,13 @@
-set -ex
-
 export WPT_PATH=/web-platform-tests
 
 git clone --depth 1 https://github.com/w3c/web-platform-tests $WPT_PATH
 
+source $WPT_PATH/tools/ci/lib.sh
+hosts_fixup
+
 export BUILD_PATH=/wptdashboard
 export RUN_PATH=battery-status
-export WPT_SHA=$(cd \$WPT_PATH && git rev-parse HEAD | head -c 10)
+export WPT_SHA=$(cd $WPT_PATH && git rev-parse HEAD | head -c 10)
 
 export PLATFORM_ID=firefox-57.0-linux
 python /wptdashboard/run/jenkins.py
